25eb20523a56621221eddf60dff29d0a0ed5f124
Stan <stanedgar@gmail.com>
2025-01-07 20:23:48 -0600

    Added btn to make new books

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function Book() {
 
 function addBookToLibrary(title, author, pages, read) {
 
+    let info = {
+        title: title, 
+        author: author, 
+        pages: pages,
+        read: read};
+
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -18,9 +24,44 @@ function addBookToLibrary(title, author, pages, read) {
         have = "read it."
     }
 
-    myLibrary.push(this.title);
+    myLibrary.push(info);
 };
 
-addBookToLibrary("Game of Thrones", "George R.R Martin", 321, true); // Adds book to array
+// addBookToLibrary("Game of Thrones", "George R.R Martin", 321, true); // Adds book to array
 
-console.log(myLibrary);
+
+
+
+
+
+// New Book
+
+let btn = document.querySelector(".btn");
+
+btn.addEventListener('click', function addBook() {
+
+    let title = prompt("Title : ");
+    let author = prompt("Author : ");
+    let pages = Number(prompt("Pages: "));
+    let read = Boolean(prompt("Read :"));
+
+    addBookToLibrary(title, author, pages, read);
+
+    let wrapper = document.querySelector(".wrapper");
+
+    //Creating cards
+    let card = document.createElement("div");
+    card.className = "card";
+    
+    wrapper.appendChild(card);
+
+    let info = document.querySelector('.card');
+
+    for (const [key, value] of Object.entries(myLibrary[0])) {
+        info.innerHTML += `${key} : ${value} <br><br>`;
+      }
+
+    myLibrary.slice(0, myLibrary.length);
+    
+
+});
